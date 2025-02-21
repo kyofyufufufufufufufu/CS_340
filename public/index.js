ab12ab77ecc6ca8bec675d21ae67b8ec3ba1eb61
Annmarie Geiger <geigerta@classwork.engr.oregonstate.edu>
2025-02-20 21:26:12 -0800

    Added inserts to Users, Books, Authors, Genres. Added pre-population to edit inputs. Added data to Book Genres page.

--- public/index.js
+++ public/index.js
@@ -1,33 +1,84 @@
-const createButton = document.getElementById('create');
-const editButton = document.getElementById('edit');
-const deleteButton = document.getElementById('delete');
-const createDialog = document.getElementById('createForm');
-const editDialog = document.getElementById('editForm');
-const deleteDialog = document.getElementById('deleteForm');
+document.addEventListener("DOMContentLoaded", function () {
+    // Track selected items (this will be reset when Edit/Delete is clicked)
+    let selectedItems = {
+        books: [],
+        users: [],
+        authors: []
+    };
 
-createButton.addEventListener('click', () => {
-	createDialog.showModal();
+    // Get buttons for books, authors, and users
+    const createBooksButton = document.getElementById('createBooksBtn');
+    const editBooksButton = document.getElementById('editBookBtn');
+    const deleteBooksButton = document.getElementById('deleteBookBtn');
+
+    const createUsersButton = document.getElementById('createUserBtn');
+    const editUsersButton = document.getElementById('editUserBtn');
+    const deleteUsersButton = document.getElementById('deleteUserBtn');
+
+    const createAuthorsButton = document.getElementById('createAuthorBtn');
+    const editAuthorsButton = document.getElementById('editAuthorBtn');
+    const deleteAuthorsButton = document.getElementById('deleteAuthorBtn');
+
+    // Get modals for books, users, and authors
+    const createBooksDialog = document.getElementById('createBookForm');
+    const editBooksDialog = document.getElementById('editBookForm');
+    const deleteBooksDialog = document.getElementById('deleteBookForm');
+
+    const createUserDialog = document.getElementById('createUserForm');
+    const editUserDialog = document.getElementById('editUserForm');
+    const deleteUserDialog = document.getElementById('deleteUserForm');
+
+    const createAuthorDialog = document.getElementById('createAuthorForm');
+    const editAuthorDialog = document.getElementById('editAuthorForm');
+    const deleteAuthorDialog = document.getElementById('deleteAuthorForm');
+
+    // Get all close buttons
+    const closeButtons = document.querySelectorAll("#closeForm");
+
+    // Open modals when buttons are clicked
+    createBooksButton?.addEventListener("click", () => createBooksDialog.showModal());
+    createUsersButton?.addEventListener("click", () => createUserDialog.showModal());
+    createAuthorsButton?.addEventListener("click", () => createAuthorDialog.showModal());
+
+    // Add event listeners for edit and delete buttons
+    editBooksButton?.addEventListener("click", () => openEditModal("book", editBooksDialog));
+    deleteBooksButton?.addEventListener("click", () => openDeleteModal("book", deleteBooksDialog));
+
+    editUsersButton?.addEventListener("click", () => openEditModal("user", editUserDialog));
+    deleteUsersButton?.addEventListener("click", () => openDeleteModal("user", deleteUserDialog));
+
+    editAuthorsButton?.addEventListener("click", () => openEditModal("author", editAuthorDialog));
+    deleteAuthorsButton?.addEventListener("click", () => openDeleteModal("author", deleteAuthorDialog));
+
+    // Close modals when close button is clicked
+    closeButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            this.closest("dialog").close();
+        });
+    });
+
+    // Track selections for books, users, and authors
+    function trackSelection(type) {
+        selectedItems[type] = [];
+        document.querySelectorAll(.select-row-${type}).forEach(row => {
+            row.addEventListener("change", function () {
+                const id = this.getAttribute(data-${type}-id);
+                if (this.checked) {
+                    selectedItems[type].push(id);
+                } else {
+                    selectedItems[type] = selectedItems[type].filter(item => item !== id);
+                }
+            });
+        });
+    }
+
+    // Capitalize the first letter of a string
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // Initialize selection tracking
+    trackSelection("book");
+    trackSelection("user");
+    trackSelection("author");
 });
-
-editButton.addEventListener('click', () => {
-	editDialog.showModal();
-});
-
-deleteButton.addEventListener('click', () => {
-	deleteDialog.showModal();
-});
-
-// Select all check box in table
-var boxes = document.getElementsByClassName('select-row');
-document.getElementById('select-all').addEventListener('change', function(e) {
-	for(var i = 0; i < boxes.length; i++)
-		boxes[i].checked = e.target.checked;
-});
-
-// De-select "select all" if one box unchecked.
-for(var i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener('change', function(e) {
-		if(!e.target.checked)
-			document.getElementById('select-all').checked = false;
-	});
-}
