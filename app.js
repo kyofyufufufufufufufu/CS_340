791601572126180e8381221861e94ead80719cc6
Annmarie Geiger <geigerta@classwork.engr.oregonstate.edu>
2025-03-13 23:14:25 -0700

    Fixed port number back to 8763

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app     = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
-PORT = 8760;
+PORT = 8763; 
 
 // Database
 const db      = require('./database/db-connector')
